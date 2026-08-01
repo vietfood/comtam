33de6fab926ab1100ccd2e64fb5141de6427eb8f
lenguyen1807 <lenguyen18072003@gmail.com>
2026-08-01 19:51:53 +0700

    [feat] add reduction kernel and scalar fast path (needs refactro)

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cmake -S . -B build \
-    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
     -DCMAKE_COLOR_DIAGNOSTICS=ON
 cmake --build build
 ./build/bin/comtam
